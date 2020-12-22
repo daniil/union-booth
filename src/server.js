2ba3a30d2e8449556ce45bb02a0180c130f0853f
Daniil Molodkov <dmolodkov@brainstation.io>
2020-12-22 14:42:18 -0500

    Add helmet headers

--- src/server.js
+++ src/server.js
@@ -2,12 +2,37 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import { v4 as uuidv4 } from 'uuid';
+import helmet from 'helmet';
+
+const app = polka();
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
+const nonceMiddleware = (_req, res, next) => {
+	res.locals = {};
+	res.locals.nonce = uuidv4();
+	next();
+};
+
+const helmetMiddleware = helmet({
+	contentSecurityPolicy: {
+		directives: {
+			defaultSrc: ["'self'", "blob: *"],
+			scriptSrc: [
+				"'self'",
+				"blob: *",
+				(_req, res) => `'nonce-${res.locals.nonce}'`
+			]
+		}
+	}
+});
+
+app
 	.use(
+		nonceMiddleware,
+		helmetMiddleware,
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
