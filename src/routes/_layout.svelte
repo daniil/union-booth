cbf526fdfa23126602ceebceea3ea0d80891f0d4
Daniil Molodkov <dmolodkov@brainstation.io>
2021-01-06 08:34:46 -0500

    Add Apollo Client to Svelte context

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,7 +1,26 @@
+<script context="module">
+  import ApolloClient from 'apollo-boost';
+	import fetch from 'isomorphic-unfetch';
+
+  export async function preload() {
+    const gqlClient = new ApolloClient({
+      name: 'Union Booth',
+      fetch,
+      credentials: 'include'
+    });
+
+    return { gqlClient };
+  }
+</script>
+
 <script>
+	import { setContext } from 'svelte';
 	import Nav from '../components/Nav.svelte';
 
 	export let segment;
+	export let gqlClient;
+
+	setContext('gqlClient', gqlClient);
 </script>
 
 <style>
