d24e9b9a427e1420cc425203f138f27de871b742
Daniil Molodkov <dmolodkov@brainstation.io>
2021-02-11 07:25:46 -0500

    Add isLive toggle for admin cohort topics

--- src/components/admin/active-cohort/ToggleLive.svelte
+++ src/components/admin/active-cohort/ToggleLive.svelte
@@ -1,9 +1,47 @@
 <script>
+  import { stores } from '@sapper/app';
+  import { mutation } from 'svelte-apollo';
+  import { TOGGLE_COHORT_TOPIC_LIVE } from 'graphql/queries/admin/active-cohort';
+  import { COHORT_TOPICS } from 'graphql/queries/admin/active-cohort'; 
+
   export let selectedCohort;
   export let details;
 
+  const { session } = stores();
+
+  const toggleCohortTopicLive = mutation(TOGGLE_COHORT_TOPIC_LIVE);
+
   const handleLiveChange = e => {
-    console.log('Live: ', e.target.checked);
+    toggleCohortTopicLive({
+      variables: {
+        cohortId: selectedCohort.id,
+        topicId: details.topic.id,
+        status: e.target.checked
+      },
+      update: (_, mutationResult) => {
+        const toggledCohortTopic = mutationResult.data.toggleCohortTopicLive;
+
+        const cohortTopics = $session.apolloClient.readQuery({
+          query: COHORT_TOPICS,
+          variables: {
+            cohortId: selectedCohort.id
+          }
+        }).cohortTopics;
+
+        $session.apolloClient.writeQuery({
+            query: COHORT_TOPICS,
+            variables: {
+              cohortId: selectedCohort.id
+            },
+            data: {
+              cohortTopics: cohortTopics.map(cohortTopic => {
+                if (cohortTopic.topic.id === toggledCohortTopic.topic.id) return toggledCohortTopic;
+                return { ...cohortTopic, isLive: false };
+              })
+            }
+          });
+      }
+    });
   }
 </script>
 
