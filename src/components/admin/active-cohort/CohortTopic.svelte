018ed93c600eb123486dd03dfaf69498e783e39e
Daniil Molodkov <dmolodkov@brainstation.io>
2021-02-11 07:14:52 -0500

    Add live styles to admin cohort topic

--- src/components/admin/active-cohort/CohortTopic.svelte
+++ src/components/admin/active-cohort/CohortTopic.svelte
@@ -16,6 +16,7 @@
     opacity: 0.6;
   }
   .wrapper h4 {
+    margin-bottom: 0;
     color: #554348;
   }
   .wrapper.is-unlocked {
@@ -24,13 +25,22 @@
   .wrapper.is-unlocked h4 {
     color: #333;
   }
+  .wrapper.is-live {
+    background-color: rgba(212, 224, 155, 0.5);
+    border: 1px solid rgb(212, 224, 155);
+    border-radius: 4px;
+  }
   .status-toggles {
     display: flex;
   }
 </style>
 
 <form action="update-cohort-topic" method="post">
-  <div class="wrapper" class:is-unlocked={details.isUnlocked}>
+  <div
+    class="wrapper"
+    class:is-unlocked={details.isUnlocked}
+    class:is-live={details.isLive}
+  >
     <h4>{details.topic.title}</h4>
     <div class="status-toggles">
       <ToggleUnlocked
