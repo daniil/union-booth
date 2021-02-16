4709353166378454f25bb0ca951d24cd8fad8a80
Daniil Molodkov <dmolodkov@brainstation.io>
2021-02-16 07:58:02 -0500

    Add CohortQuestion associations

--- src/graphql/models/associations.js
+++ src/graphql/models/associations.js
@@ -11,22 +11,29 @@ export const setupAssociations = models => {
   models.Program.belongsTo(models.User);
 
   models.Cohort.hasMany(models.CohortTopic, { onDelete: 'cascade', hooks: true });
+  models.Cohort.hasMany(models.CohortQuestion, foreignKeyNonNull);
   models.Cohort.hasMany(models.User, { constraints: false });
   models.Cohort.belongsTo(models.User);
   models.Cohort.belongsTo(models.Program);
   models.Cohort.belongsToMany(models.Topic, { through: models.CohortTopic });
 
-  models.Topic.hasMany(models.CohortTopic);
+  models.Topic.hasMany(models.CohortTopic, foreignKeyNonNull);
+  models.Topic.hasMany(models.CohortQuestion, foreignKeyNonNull);
   models.Topic.belongsTo(models.User);
   models.Topic.belongsTo(models.Program);
   models.Topic.belongsToMany(models.Cohort, { through: models.CohortTopic });
 
   models.CohortTopic.belongsTo(models.Cohort);
   models.CohortTopic.belongsTo(models.Topic);
+
+  models.CohortQuestion.belongsTo(models.Cohort);
+  models.CohortQuestion.belongsTo(models.Topic);
+  models.CohortQuestion.belongsTo(models.User);
   
   models.User.hasMany(models.Program, foreignKeyNonNull);
   models.User.hasMany(models.Cohort, foreignKeyNonNull);
   models.User.hasMany(models.Topic, foreignKeyNonNull);
+  models.User.hasMany(models.CohortQuestion, foreignKeyNonNull);
   models.User.belongsTo(models.Program, { foreignKey: 'selectedProgram', constraints: false });
   models.User.belongsTo(models.Cohort, { constraints: false });
 }
--- src/graphql/models/cohort-question.js
+++ src/graphql/models/cohort-question.js
@@ -1,5 +1,13 @@
+import Sequelize from 'sequelize';
+
 const cohortQuestion = (sequelize, DataTypes) => {
   const CohortQuestion = sequelize.define('cohort-question', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+      unique: true
+    },
     isAnonymous: {
       type: DataTypes.BOOLEAN,
       defaultValue: false
