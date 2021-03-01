31fbea3b81ff9d40db0c95a59ed6e990672d4986
Daniil Molodkov <dmolodkov@brainstation.io>
2021-03-01 07:04:38 -0500

    Add cohort-answer client queries

---
+++ src/graphql/queries/cohort-answer.js
@@ -0,0 +1,36 @@
+import gql from 'graphql-tag';
+
+const cohortAnswerInformation = gql`
+  fragment CohortAnswerInformation on CohortAnswer {
+    id
+    answer
+    createdAt
+    user {
+      id
+      username
+    }
+  }
+`;
+
+const LIVE_ANSWERS = gql`
+  query LiveAnswers($cohortQuestionId: ID!) {
+    liveAnswers(cohortQuestionId: $cohortQuestionId) {
+      ...CohortAnswerInformation
+    }
+  }
+  ${cohortAnswerInformation}
+`;
+
+const ADD_COHORT_ANSWER = gql`
+  mutation AddCohortAnswer($cohortQuestionId:ID!, $answer:String!) {
+  addCohortAnswer(cohortQuestionId: $cohortQuestionId, answer: $answer) {
+    ...CohortAnswerInformation
+    }
+  }
+  ${cohortAnswerInformation}
+`;
+
+export {
+  LIVE_ANSWERS,
+  ADD_COHORT_ANSWER
+}
