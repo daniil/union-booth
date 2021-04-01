8e2e7a934dc51d89df609cbb2d18c404cac1d2ba
Daniil Molodkov <dmolodkov@brainstation.io>
2021-04-01 07:53:35 -0400

    Add topics query to FAQs page

--- src/routes/admin/faqs/index.svelte
+++ src/routes/admin/faqs/index.svelte
@@ -2,6 +2,70 @@
 	<title>Union Booth :: Admin - FAQs</title>
 </svelte:head>
 
+<script context="module">
+  import { SELECTED_PROGRAM } from 'graphql/queries/admin/programs';
+  import { TOPICS } from 'graphql/queries/admin/topics';
+
+  export async function preload(_, session) {
+    try {
+      const selectedProgramQuery = await session.apolloClient.query({ query: SELECTED_PROGRAM });
+      await session.apolloClient.query({
+        query: TOPICS,
+        variables: {
+          programId: selectedProgramQuery.data.selectedProgram.id
+        }
+      });
+    } catch(err) {};
+  }
+</script>
+
+<script>
+  import { stores } from '@sapper/app';
+  import { onDestroy } from 'svelte';
+
+  const { session } = stores();
+
+  let selectedProgram = $session.apolloClient.readQuery({ query: SELECTED_PROGRAM }).selectedProgram;
+
+  const selectedProgramSub = $session.apolloClient
+    .watchQuery({
+      query: SELECTED_PROGRAM,
+      fetchPolicy: 'cache-and-network'
+    })
+    .subscribe(({ data }) => {
+      selectedProgram = data.selectedProgram;
+    });
+
+  onDestroy(() => selectedProgramSub.unsubscribe());
+  
+  let topics = [];
+
+  $: if (selectedProgram) {
+    try {
+      topics = $session.apolloClient.readQuery({
+        query: TOPICS,
+        variables: {
+          programId: selectedProgram.id
+        }
+      }).topics;
+    } catch(e) {}
+
+    const topicsSub = $session.apolloClient
+      .watchQuery({
+        query: TOPICS,
+        variables: {
+          programId: selectedProgram.id
+        },
+        fetchPolicy: 'cache-and-network'
+      })
+      .subscribe(({ data }) => {
+        topics = data.topics;
+      });
+
+    onDestroy(() => topicsSub.unsubscribe());
+  }
+</script>
+
 <style>
   section {
     margin-bottom: 2rem;
@@ -13,3 +77,12 @@
 </style>
 
 <h2>FAQs</h2>
+
+{#if selectedProgram}
+  <section>
+    <h3><span class="selected-program">{selectedProgram.title}</span> Topics</h3>
+    {topics}
+  </section>
+{:else}
+  <p>No active program selected currently 🙍🏼‍♂️. Please <a rel="prefetch" href="/admin/programs">select one</a></p>
+{/if}
