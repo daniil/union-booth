b1161d9cf80bfa2995dfbc79aadea73b944e9ad6
Daniil Molodkov <dmolodkov@brainstation.io>
2021-04-07 15:17:21 -0400

    Add initial styling of the QAEditor

--- src/components/admin/faqs/QAEditor.svelte
+++ src/components/admin/faqs/QAEditor.svelte
@@ -7,8 +7,20 @@
   let animationConfig = { y: 200, duration: 500 };
 </script>
 
+<style>
+  .qa-editor {
+    position: fixed;
+    padding: 2rem;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #fff;
+  }
+</style>
+
 {#if visible}
   <section
+    class="qa-editor"
     in:fly="{{...animationConfig, easing: cubicOut}}"
     out:fly="{{...animationConfig, easing: cubicIn}}"
   >
