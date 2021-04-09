a2d14f751af8de71b68174ba630bedd160b4b069
Daniil Molodkov <dmolodkov@brainstation.io>
2021-04-09 16:37:25 -0400

    Refactor QAEditor animation to CSS animation

--- src/components/admin/faqs/QAEditor.svelte
+++ src/components/admin/faqs/QAEditor.svelte
@@ -1,11 +1,8 @@
 <script>
-  import { fly } from 'svelte/transition';
-  import { cubicOut, cubicIn } from 'svelte/easing';
   import MDEditor from 'components/MDEditor.svelte';
 
   export let visible;
 
-  let animationConfig = { y: 200, duration: 500 };
   let editorValues = {
     question: '',
     answer: ''
@@ -26,29 +23,31 @@
     background-color: #fff;
     box-shadow: rgb(0 0 0 / 15%) 0px 10px 60px;
     border-top: 1px solid rgba(0, 0, 0, 0.05);
+    transform: translateY(100%);
+    transition: transform 0.5s cubic-bezier(0.65, 0, 0.35, 1);
+  }
+  .visible {
+    transform: translateY(0%);
   }
 </style>
 
-{#if visible}
-  <section
-    class="qa-editor"
-    in:fly="{{...animationConfig, easing: cubicOut}}"
-    out:fly="{{...animationConfig, easing: cubicIn}}"
-  >
-    <h2>QA Editor</h2>
-    <MDEditor
-      id="question"
-      label="Question"
-      placeholder="Question details"
-      value={editorValues.question}
-      on:change={handleEditorChange}
-    />
-    <MDEditor
-      id="answer"
-      label="Answer"
-      placeholder="Answer details"
-      value={editorValues.answer}
-      on:change={handleEditorChange}
-    />
-  </section>
-{/if}
+<section
+  class="qa-editor"
+  class:visible
+>
+  <h2>QA Editor</h2>
+  <MDEditor
+    id="question"
+    label="Question"
+    placeholder="Question details"
+    value={editorValues.question}
+    on:change={handleEditorChange}
+  />
+  <MDEditor
+    id="answer"
+    label="Answer"
+    placeholder="Answer details"
+    value={editorValues.answer}
+    on:change={handleEditorChange}
+  />
+</section>
