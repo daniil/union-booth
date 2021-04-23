4905c71abc74ae4c206dc20a4a6d3ee8e6cad140
Daniil Molodkov <dmolodkov@brainstation.io>
2021-04-23 14:49:06 -0400

    Add production path resolution for avatars

--- src/utils/generateAvatar.js
+++ src/utils/generateAvatar.js
@@ -3,10 +3,14 @@ import multiavatar from '@multiavatar/multiavatar';
 import fs from 'fs';
 import path from 'path';
 
-const avatarsFolder = path.resolve(__dirname, '../../../static/avatars');
+const { NODE_ENV } = process.env;
+
+const avatarsFolder = NODE_ENV === 'production'
+  ? path.resolve(__dirname, './static/avatars')
+  : path.resolve(__dirname, '../../../static/avatars');
 
 if (!fs.existsSync(avatarsFolder)){
-  fs.mkdirSync(avatarsFolder);
+  fs.mkdirSync(avatarsFolder, { recursive: true });
 }
 
 export default userId => {
