06a03b57675d4b6bf33fc5dc6c748acef25d3e46
Daniil Molodkov <dmolodkov@brainstation.io>
2021-04-30 16:14:55 -0400

    Add topic-faq client side topic page  query

---
+++ src/graphql/queries/topic-faq.js
@@ -0,0 +1,21 @@
+import gql from 'graphql-tag';
+
+const TOPIC_FAQ = gql`
+  query TopicFAQ($slug: ID!) {
+    topicFAQ(slug: $slug) {
+      topic {
+        title
+      }
+      topicFAQQuestions {
+        id
+        question
+        answer
+        createdAt
+      }
+    }
+  }
+`;
+
+export {
+  TOPIC_FAQ
+}
