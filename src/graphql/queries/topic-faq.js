418bc133c6f6cb9938015c92cab34928cac3edb6
Daniil Molodkov <dmolodkov@brainstation.io>
2021-04-30 16:40:38 -0400

    Add TOPIC_FAQ query to faq page

--- src/graphql/queries/topic-faq.js
+++ src/graphql/queries/topic-faq.js
@@ -4,6 +4,7 @@ const TOPIC_FAQ = gql`
   query TopicFAQ($slug: ID!) {
     topicFAQ(slug: $slug) {
       topic {
+        id
         title
       }
       topicFAQQuestions {
--- src/routes/topics/[slug]/faqs.svelte
+++ src/routes/topics/[slug]/faqs.svelte
@@ -1,5 +1,46 @@
-<!-- <svelte:head>
-	<title>Union Booth :: Topic: {cohortTopic.topic ? cohortTopic.topic.title : 'Loading'} - FAQs</title>
-</svelte:head> -->
+<svelte:head>
+	<title>Union Booth :: Topic: {topicFAQ.topic ? topicFAQ.topic.title : 'Loading'} - FAQs</title>
+</svelte:head>
+
+<script context="module">
+  import { TOPIC_FAQ } from 'graphql/queries/topic-faq';
+
+  export async function preload(page, session) {
+    await session.apolloClient.query({
+      query: TOPIC_FAQ,
+      variables: { slug: page.params.slug }
+    });
+
+    return {
+      slug: page.params.slug
+    };
+  }
+</script>
+
+<script>
+  import { stores } from '@sapper/app';
+  import { onDestroy } from 'svelte';
+
+  export let slug;
+
+  const { session } = stores();
+
+  let topicFAQ = $session.apolloClient.readQuery({
+    query: TOPIC_FAQ,
+    variables: { slug }
+  }).topicFAQ;
+
+  const topicFAQSub = $session.apolloClient
+    .watchQuery({
+      query: TOPIC_FAQ,
+      variables: { slug },
+      fetchPolicy: 'cache-and-network'
+    })
+    .subscribe(({ data }) => {
+      topicFAQ = data.topicFAQ;
+    });
+  
+  onDestroy(() => topicFAQSub.unsubscribe());
+</script>
 
 <h3>Frequently Asked Questions Content</h3>
