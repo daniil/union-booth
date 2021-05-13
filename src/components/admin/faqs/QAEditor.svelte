29fe2b713f1389df35d16fb98b5cf87e60594f2a
Daniil Molodkov <dmolodkov@brainstation.io>
2021-05-13 16:13:45 -0400

    Refactor QAEditor to use SlideOutPanel component

--- src/components/admin/faqs/QAEditor.svelte
+++ src/components/admin/faqs/QAEditor.svelte
@@ -2,6 +2,7 @@
   import { stores } from '@sapper/app';
   import { mutation } from 'svelte-apollo';
   import { createEventDispatcher } from 'svelte';
+  import SlideOutPanel from 'components/SlideOutPanel.svelte';
   import MDEditor from 'components/MDEditor.svelte';
   import Button from 'components/Button.svelte';
   import { TOPIC_FAQ_ADMIN, ADD_TOPIC_FAQ_QUESTION } from 'graphql/queries/admin/topic-faq';
@@ -135,48 +136,11 @@
   }
 </script>
 
-<style>
-  .qa-editor {
-    position: fixed;
-    padding: 2rem;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #fff;
-    box-shadow: rgb(0 0 0 / 15%) 0px 10px 60px;
-    border-top: 1px solid rgba(0, 0, 0, 0.05);
-    transform: translateY(100%);
-    transition: transform 0.5s cubic-bezier(0.65, 0, 0.35, 1);
-  }
-  h2 {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .close {
-    padding: 0.5rem 0.75rem;
-    border: 1px solid #D4C4C7;
-    border-radius: 50%;
-    background: none;
-    font-size: 1rem;
-    color: #9B6A6C;
-    cursor: pointer;
-  }
-  .close:hover {
-    background-color: #F6F3F4;
-    border-color: #B09398;
-    color: #B09398;
-  }
-  .visible {
-    transform: translateY(0%);
-  }
-</style>
-
-<section class="qa-editor" class:visible>
-  <h2>
-    QA Editor
-    <button class="close" on:click={triggerClose}>X</button>
-  </h2>
+<SlideOutPanel
+  title="QA Editor"
+  on:close
+  {visible}
+>
   <MDEditor
     id="question"
     label="Question"
@@ -192,4 +156,4 @@
     on:change={handleEditorChange}
   />
   <Button variant="success" icon="👍" label="Save" action={saveQA} disabled={submitDisabled}/>
-</section>
+</SlideOutPanel>
