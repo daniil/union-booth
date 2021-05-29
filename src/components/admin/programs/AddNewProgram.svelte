1611c3220d724d3974ad94242ed85c78f3327a96
Daniil Molodkov <dmolodkov@brainstation.io>
2021-05-29 07:08:49 -0400

    Update styling for AddNewProgram form

--- src/components/admin/programs/AddNewProgram.svelte
+++ src/components/admin/programs/AddNewProgram.svelte
@@ -2,18 +2,20 @@
   import { stores } from '@sapper/app';
   import { mutation } from 'svelte-apollo';
   import { PROGRAMS, ADD_PROGRAM } from 'graphql/queries/admin/programs';
+  import TextInput from 'components/TextInput.svelte';
+  import Button from 'components/Button.svelte';
 
   const { session } = stores();
   
   const addProgram = mutation(ADD_PROGRAM);
 
-  let programEl;
+  let programName;
 
   const handleSubmit = async e => {
     try {
       await addProgram({
         variables: {
-          title: e.target.program.value
+          title: programName
         },
         update: (_, mutationResult) => {
           const programs = $session.apolloClient.readQuery({ query: PROGRAMS }).programs;
@@ -27,17 +29,29 @@
         }
       });
 
-      programEl.value = '';
+      programName = '';
     } catch(err) {
       console.log('ERROR: ', err);
     }
   }
 </script>
 
+<style>
+  .form-element {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+  .input-wrapper {
+    width: 75%;
+  }
+</style>
+
 <form action="/add-program" method="post" on:submit|preventDefault={handleSubmit}>
   <div class="form-element">
-    <label for="program">Program Name: </label>
-    <input bind:this={programEl} type="text" name="program" id="program" placeholder="Program Name" required>
-    <input type="submit" value="Create">
+    <div class="input-wrapper">
+      <TextInput id="program" type="text" label="Program Name" bind:value={programName} required/>
+    </div>
+    <Button type="submit" icon="📖" label="Create"/>
   </div>
 </form>
