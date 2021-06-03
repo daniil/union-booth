27ba439edf3825601ee3f7a033c82288bbfe5675
Daniil Molodkov <dmolodkov@brainstation.io>
2021-06-03 15:02:50 -0400

    Add initial resources query to individual topic resources section

--- src/routes/topics/[slug]/resources.svelte
+++ src/routes/topics/[slug]/resources.svelte
@@ -1,5 +1,46 @@
-<!-- <svelte:head>
-	<title>Union Booth :: Topic: {cohortTopic.topic ? cohortTopic.topic.title : 'Loading'} - Resources</title>
-</svelte:head> -->
+<svelte:head>
+	<title>Union Booth :: Topic: {topicResources.topic ? topicResources.topic.title : 'Loading'} - Resources</title>
+</svelte:head>
+
+<script context="module">
+  import { TOPIC_RESOURCES } from 'graphql/queries/topic-resources';
+
+  export async function preload(page, session) {
+    await session.apolloClient.query({
+      query: TOPIC_RESOURCES,
+      variables: { slug: page.params.slug }
+    });
+
+    return {
+      slug: page.params.slug
+    }
+  }
+</script>
+
+<script>
+  import { stores } from '@sapper/app';
+  import { onDestroy } from 'svelte';
+
+  export let slug;
+
+  const { session } = stores();
+
+  let topicResources = $session.apolloClient.readQuery({
+    query: TOPIC_RESOURCES,
+    variables: { slug }
+  }).topicResources;
+
+  const topicResourcesSub = $session.apolloClient
+    .watchQuery({
+      query: TOPIC_RESOURCES,
+      variables: { slug },
+      fetchPolicy: 'cache-and-network'
+    })
+    .subscribe(({ data }) => {
+      topicResources = data.topicResources;
+    });
+  
+  onDestroy(() => topicResourcesSub.unsubscribe());
+</script>
 
 <h3>Resources Content</h3>
