28a8f0a62b48edcbbbd47749406a27d3d500b789
Daniil Molodkov <dmolodkov@brainstation.io>
2021-06-17 15:51:53 -0400

    Add UPDATE_USER_PASSWORD functionality to UpdatePassword component

--- src/components/UpdatePassword.svelte
+++ src/components/UpdatePassword.svelte
@@ -1,27 +1,43 @@
 <script>
-  import parseError from 'utils/parseError';
+  import { mutation } from 'svelte-apollo';
+  import { UPDATE_USER_PASSWORD } from 'graphql/queries/user';
   import FormErrors from 'components/FormErrors.svelte';
   import TextInput from 'components/TextInput.svelte';
   import Button from 'components/Button.svelte';
 
   export let user;
 
+  const updateUserPassword = mutation(UPDATE_USER_PASSWORD);
+
+  let password;
+  let passwordConfirm;
   let errors;
   let formDisabled = false;
 
   $: buttonVariant = formDisabled ? 'loading' : 'success';
 
-  const handleUpdate = e => {
-    if (e.target.password.value !== e.target.passwordConfirm.value) {
+  const handleUpdate = async e => {
+    if (password !== passwordConfirm) {
       errors = '⚠️ Passwords do not match';
       return;
     }
 
     try {
-      console.log('Value: ', e.target.password.value);
-    } catch(err) {
-      errors = null;
       formDisabled = true;
+
+      await updateUserPassword({
+        variables: {
+          userId: user.id,
+          password: password
+        }
+      });
+
+      formDisabled = false;
+      password = '';
+      passwordConfirm = '';
+    } catch(err) {
+      formDisabled = true;
+      errors = '⚠️ Could not update the password';
     }
   }
 </script>
@@ -51,10 +67,10 @@
   <h2>Update Password</h2>
   <form action="/update" method="post" on:submit|preventDefault={handleUpdate}>
     <div class="form-element">
-      <TextInput id="password" type="password" label="New Password" required disabled={formDisabled}/>
+      <TextInput id="password" type="password" label="New Password" bind:value={password} required disabled={formDisabled}/>
     </div>
     <div class="form-element">
-      <TextInput id="passwordConfirm" type="password" label="Confirm Password" required disabled={formDisabled}/>
+      <TextInput id="passwordConfirm" type="password" label="Confirm Password" bind:value={passwordConfirm} required disabled={formDisabled}/>
     </div>
     <div class="form-element action">
       <Button type="submit" variant={buttonVariant} icon="🪄" label="Update"/>
