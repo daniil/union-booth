42ef471ef2cc9e1d13ca1803a810e9b7095576aa
Daniil Molodkov <dmolodkov@brainstation.io>
2021-06-30 16:46:34 -0400

    Add AnswerActionsNav component

--- src/components/AnswerActions.svelte
+++ src/components/AnswerActions.svelte
@@ -2,6 +2,7 @@
   import { fly } from 'svelte/transition';
   import { quintOut } from 'svelte/easing';
   import MeatballIcon from 'components/MeatballIcon.svelte';
+  import AnswerActionsNav from 'components/AnswerActionsNav.svelte';
 
   export let answerId;
   export let isOwner;
@@ -11,6 +12,10 @@
   const handleActionDropdown = () => {
     navVisible = !navVisible;
   }
+
+  const handleActionComplete = () => {
+    navVisible = false;
+  }
 </script>
 
 <style lang="scss">
@@ -18,13 +23,15 @@
     background: transparent;
     border: none;
     cursor: pointer;
+    padding: 1rem 0;
+    margin-top: -1rem;
     &:hover {
       filter: brightness(1.25);
     }
   }
   .answer-actions-nav-wrapper {
     position: absolute;
-    top: 90%;
+    top: 50%;
     right: 0;
   }
 </style>
@@ -34,6 +41,10 @@
 </button>
 {#if navVisible}
   <div class="answer-actions-nav-wrapper" transition:fly="{{ duration: 300, y: 5, opacity: 0, easing: quintOut }}">
-    Answer Nav
+    <AnswerActionsNav
+      {answerId}
+      {isOwner}
+      on:action-complete={handleActionComplete}
+    />
   </div>
 {/if}
---
+++ src/components/AnswerActionsNav.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  import Button from 'components/Button.svelte';
+
+  export let answerId;
+  export let isOwner;
+
+  const dispatch = createEventDispatcher();
+  
+  const handleEdit = () => {
+    dispatch('action-complete');
+  }
+
+  const handleDeactivate = () => {
+    dispatch('action-complete');
+  }
+</script>
+
+<style lang="scss">
+  .question-actions-nav {
+    display: flex;
+    padding: 0.75rem;
+    background-color: #fff;
+    box-shadow: rgb(0 0 0 / 10%) 0px 4px 12px;
+    border-radius: 4px;
+  }
+</style>
+
+<nav class="question-actions-nav">
+  {#if isOwner}
+    <Button style="link" label="Edit" action={handleEdit}/>
+  {/if}
+  <Button style="link" variant="danger" label="Delete" action={handleDeactivate}/>
+</nav>
