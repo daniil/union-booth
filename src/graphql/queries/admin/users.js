0984f2f3022825f3aeacba17dd5ac22123e05766
Daniil Molodkov <dmolodkov@brainstation.io>
2021-07-22 08:53:38 -0400

    Refactor userInformation to a fragment for front end admin users queries

--- src/graphql/queries/admin/users.js
+++ src/graphql/queries/admin/users.js
@@ -1,16 +1,23 @@
 import gql from 'graphql-tag';
 
+const userInformation = gql`
+  fragment UserInformation on User {
+    id
+    firstName
+    lastName
+    username
+    email
+    createdAt
+  }
+`;
+
 const COHORT_USERS = gql`
   query CohortUsers($cohortId: ID!) {
     cohortUsers(cohortId: $cohortId) {
-      id
-      firstName
-      lastName
-      username
-      email
-      createdAt
+      ...UserInformation
     }
   }
+  ${userInformation}
 `;
 
 export {
