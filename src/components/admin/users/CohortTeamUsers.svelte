9390ae2d0880a1ac3e149e955d4540b719b5d35e
Daniil Molodkov <dmolodkov@brainstation.io>
2021-07-23 08:11:34 -0400

    Add CohortTeamUsers FE queries and initial users tables

--- src/components/admin/users/CohortTeamUsers.svelte
+++ src/components/admin/users/CohortTeamUsers.svelte
@@ -1,1 +1,48 @@
-<p>Cohort Team Users</p>
+<script>
+  import { stores } from '@sapper/app';
+  import { onDestroy } from 'svelte';
+  import { COHORT_TEAM_USERS } from 'graphql/queries/admin/users';
+  import Loading from 'components/shared/Loading.svelte';
+  import CohortUsersHeader from 'components/admin/users/CohortUsersHeader.svelte';
+  import CohortUser from 'components/admin/users/CohortUser.svelte';
+
+  export let cohortId;
+
+  const { session } = stores();
+
+  let users = [];
+  let loading = true;
+
+  $: usersSub = watchUsersQuery(cohortId);
+
+  const watchUsersQuery = cohortId => {
+    return $session.apolloClient
+      .watchQuery({
+        query: COHORT_TEAM_USERS,
+        variables: {
+          cohortId
+        },
+        fetchPolicy: 'cache-and-network'
+      })
+      .subscribe(({ data }) => {
+        if (!data) return;
+        users = data.cohortTeamUsers;
+        loading = false;
+      })
+  }
+
+  onDestroy(() => usersSub.unsubscribe());
+</script>
+
+{#if loading}
+  <Loading/>
+{:else}
+  {#if users.length}
+    <CohortUsersHeader/>
+  {/if}
+  {#each users as user (user.id)}
+    <CohortUser {user}/>
+  {:else}
+    <p>No users yet.</p>
+  {/each}
+{/if}
