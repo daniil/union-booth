02e58b7bee0a78823e2594bff82497f78c6ba0eb
Daniil Molodkov <dmolodkov@brainstation.io>
2021-07-24 10:08:42 -0400

    Create KebabIcon component. Add initial KebabIcon button to CohortUser

--- src/components/admin/users/CohortUser.svelte
+++ src/components/admin/users/CohortUser.svelte
@@ -1,15 +1,22 @@
 <script>
   import { formatDate } from 'utils/time';
+  import KebabIcon from 'components/icons/KebabIcon.svelte';
 
   export let user;
 
   $: createdDate = formatDate(user.createdAt, true);
+
+  const handleActionDropdown = () => {
+    console.log('Handle dropdown');
+  }
 </script>
 
 <style lang="scss">
   .user {
+    position: relative;
     display: flex;
     justify-content: space-between;
+    align-items: center;
     padding: 0.5rem 0.5rem;
     margin: 0.25rem 0;
     font-size: 0.85rem;
@@ -27,6 +34,16 @@
       width: 16%;
     }
   }
+  .trigger-action {
+    position: absolute;
+    right: -4px;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    &:hover {
+      filter: brightness(1.25);
+    }
+  }
 </style>
 
 <p class="user">
@@ -35,4 +52,7 @@
   <span class="cell username" title={user.username}>{user.username}</span>
   <span class="cell email" title={user.email}>{user.email}</span>
   <span class="cell date-joined" title={createdDate}>{createdDate}</span>
+  <button class="trigger-action" click={handleActionDropdown}>
+    <KebabIcon color="#05668D"/>
+  </button>
 </p>
---
+++ src/components/icons/KebabIcon.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let color;
+  export let label;
+</script>
+
+<style lang="scss">
+  $ball-size: 4px;
+  $offset: 2px;
+  $ball-color: currentColor;
+  @mixin ball() {
+    display: block;
+    content: '';
+    width: $ball-size;
+    height: $ball-size;
+    border-radius: 50%;
+    background-color: $ball-color;
+  }
+  .icon-container {
+    display: flex;
+    justify-content: center;
+    width: ($ball-size * 2) + ($offset * 2);
+    height: ($ball-size * 3) + ($offset * 2);
+  }
+  .meatball-icon {
+    position: relative;
+    @include ball;
+    &::before {
+      position: absolute;
+      top: $ball-size + $offset;
+      @include ball;
+    }
+    &::after {
+      position: absolute;
+      top: ($ball-size + $offset) * 2;
+      @include ball;
+    }
+  }
+</style>
+
+<span class="icon-container">
+  <span class="meatball-icon" role="img" aria-label={label} style="color: {color}"></span>
+</span>
