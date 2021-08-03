bf9ce7210cefb5506ec727ae7d5259c9aef0210e
Daniil Molodkov <dmolodkov@brainstation.io>
2021-08-03 09:21:17 -0400

    Add preloader to deactivating account functionality

--- src/components/profile/DeactivateAccount.svelte
+++ src/components/profile/DeactivateAccount.svelte
@@ -1,15 +1,20 @@
 <script>
   import { goto, stores } from '@sapper/app';
-  import ConfirmationButton from 'components/forms/ConfirmationButton.svelte';
   import { mutation } from 'svelte-apollo';
   import { DEACTIVATE_USER_ACCOUNT } from 'graphql/queries/user';
+  import ConfirmationButton from 'components/forms/ConfirmationButton.svelte';
+  import Loading from 'components/shared/Loading.svelte';
 
   const { session } = stores();
 
   const deactivateUserAccount = mutation(DEACTIVATE_USER_ACCOUNT);
 
+  let loading = false;
+
   const handleDeactivate = async () => {
+    loading = true;
     await deactivateUserAccount();
+    loading = false;
     $session.user = null;
 		goto('/');
   }
@@ -19,9 +24,20 @@
   .deactivate-container {
     display: flex;
     justify-content: center;
+    position: relative;
+  }
+  .loading-container {
+    position: absolute;
+    top: -6px;
   }
 </style>
 
 <div class="deactivate-container">
-  <ConfirmationButton style="link" variant="danger" label="Deactivate Account" action={handleDeactivate}/>
+  {#if loading}
+    <div class="loading-container">
+      <Loading/>
+    </div>
+  {:else}
+    <ConfirmationButton style="link" variant="danger" label="Deactivate Account" action={handleDeactivate}/>
+  {/if}
 </div>
