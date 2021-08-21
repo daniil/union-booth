0d812f7a0afadf5d2778199824d3413c54c783fe
Daniil Molodkov <dmolodkov@brainstation.io>
2021-08-21 08:25:12 -0400

    Add requires verification illustration

--- src/components/shared/WaitingVerification.svelte
+++ src/components/shared/WaitingVerification.svelte
@@ -5,6 +5,11 @@
     color: #7D6D61;
     font-size: 1.1rem;
   }
+  .verification-illustration {
+    display: block;
+    width: 350px;
+    margin: 0 auto 4rem;
+  }
   .verification-detail {
     text-align: center;
     color: #7D6D61;
@@ -12,5 +17,6 @@
   }
 </style>
 
-<p class="verification-msg">This account requires verification.</p>
-<p class="verification-detail">Please wait for the admin of the cohort to activate your account.</p>
+<p class="verification-msg">This account requires verification</p>
+<img src="images/needs-verification.svg" alt="Woman waiting for task to complete" class="verification-illustration"/>
+<p class="verification-detail">Please wait for the admin of the cohort to activate your account</p>
