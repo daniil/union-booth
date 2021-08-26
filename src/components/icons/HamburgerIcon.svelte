a0b3d3291c627731a75662cb7c32a5c6974925d1
Daniil Molodkov <dmolodkov@brainstation.io>
2021-08-26 09:27:20 -0400

    Add HamburgerIcon component. Add initial mobile nav button

---
+++ src/components/icons/HamburgerIcon.svelte
@@ -0,0 +1,43 @@
+<script>
+  export let color;
+  export let label;
+</script>
+
+<style lang="scss">
+  $line-width: 20px;
+  $line-height: 3px;
+  $offset: 3px;
+  $line-color: currentColor;
+  @mixin line() {
+    display: block;
+    content: '';
+    width: $line-width;
+    height: $line-height;
+    border-radius: 4px;
+    background-color: $line-color;
+  }
+  .icon-container {
+    display: flex;
+    justify-content: center;
+    width: $line-width;
+    height: ($line-height * 3) + ($offset * 2);
+  }
+  .hamburger-icon {
+    position: relative;
+    @include line;
+    &::before {
+      position: absolute;
+      top: $line-height + $offset;
+      @include line;
+    }
+    &::after {
+      position: absolute;
+      top: ($line-height + $offset) * 2;
+      @include line;
+    }
+  }
+</style>
+
+<span class="icon-container">
+  <span class="hamburger-icon" role="img" aria-label={label} style="color: {color}"></span>
+</span>
--- src/components/layout/MobileNav.svelte
+++ src/components/layout/MobileNav.svelte
@@ -1,1 +1,18 @@
-Mobile Nav
+<script>
+  import HamburgerIcon from "components/icons/HamburgerIcon.svelte";
+</script>
+
+<style lang="scss">
+  .mobile-nav-btn {
+    padding: 1.27rem 2rem;
+    border: none;
+    background-color: transparent;
+    &:hover {
+      filter: brightness(1.25);
+    }
+  }
+</style>
+
+<button class="mobile-nav-btn">
+  <HamburgerIcon color="#05668D" label="Site Navigation"/>
+</button>
