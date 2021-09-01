ced06726a0d277dcb6b0154a34118b86c2d57847
Daniil Molodkov <dmolodkov@brainstation.io>
2021-09-01 09:47:55 -0400

    Add JSDoc for elementClickOff util

--- src/utils/elementClickOff.js
+++ src/utils/elementClickOff.js
@@ -1,4 +1,9 @@
-// TODO: Add parameter explanation
+/**
+ * @param {Object} event - Event object from event handler
+ * @param {string} elementClass - A classname CSS selector for an element that should ignore the click off (ie: the trigger of the state that click off is updating in it's callback, such as button container that would open a menu)
+ * @param {string} elementId - A unique id of the element that is ignored (ie: if you have multiples of the same elementClass, such as action buttons for multiple similar elements on the page)
+ * @param {Function} stateCb - A callback function for successful click off (ie: if click happened outside of elementClass element or it's children)
+ */
 export default (event, elementClass, elementId, stateCb) => {
   let currTarget = event.target;
   while (currTarget.parentNode) {
