a05575832734c46eeed1d61f1b3405aa22e1df59
Daniil Molodkov <dmolodkov@brainstation.io>
2021-09-13 09:07:57 -0400

    Add initial unverified users page

--- src/components/admin/users/UnverifiedUsers.svelte
+++ src/components/admin/users/UnverifiedUsers.svelte
@@ -1,1 +1,55 @@
-<p>Unverified users</p>
+<script>
+  import { stores } from '@sapper/app';
+  import { onDestroy } from 'svelte';
+  import { UNVERIFIED_USERS } from 'graphql/queries/admin/users';
+  import Loading from 'components/shared/Loading.svelte';
+  import UsersHeader from 'components/admin/users/UsersHeader.svelte';
+  import UserRow from 'components/admin/users/UserRow.svelte';
+
+  export let cohortId;
+
+  const { session } = stores();
+
+  let users = [];
+  let loading = true;
+
+  $: usersSub = watchUsersQuery();
+
+  const watchUsersQuery = () => {
+    return $session.apolloClient
+      .watchQuery({
+        query: UNVERIFIED_USERS,
+        variables: {
+          cohortId
+        },
+        fetchPolicy: 'cache-and-network'
+      })
+      .subscribe(({ data }) => {
+        if (!data) return;
+        users = data.unverifiedUsers;
+        loading = false;
+      })
+  }
+
+  onDestroy(() => usersSub.unsubscribe());
+</script>
+
+<style lang="scss">
+  @use '../../../styles/partials/admin';
+  .no-users {
+    @extend %no-users;
+  }
+</style>
+
+{#if loading}
+  <Loading/>
+{:else}
+  {#if users.length}
+    <UsersHeader/>
+  {/if}
+  {#each users as user (user.id)}
+    <UserRow {user}/>
+  {:else}
+    <p class="no-users">No users left to verify 👌</p>
+  {/each}
+{/if}
