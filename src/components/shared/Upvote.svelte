0c262cac97ca90385a366b82e60e6c475bff04c9
Daniil Molodkov <dmolodkov@brainstation.io>
2021-09-21 09:07:41 -0400

    Add initial vote counter to Upvote component

--- src/components/shared/Upvote.svelte
+++ src/components/shared/Upvote.svelte
@@ -4,21 +4,33 @@
 
 <style lang="scss">
   .upvote {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     cursor: pointer;
-    color: transparent;
+    position: relative;
+    top: 0.1rem;
+    right: -0.4rem;
+    font-size: 0.65rem;
+    font-weight: 500;
+    color: #3E6990;
+    @media (min-width: 48rem) {
+      font-size: 0.75rem;
+    }
     &:hover {
       filter: brightness(1.25);
     }
     :global(svg) {
       fill: #3E6990;
-      position: relative;
-      top: 0.1rem;
-      right: -0.7rem;
-      width: 35px;
+      width: 25px;
+    }
+    .counter {
+      margin-top: -0.1rem;
     }
   }
 </style>
 
 <div class="upvote" on:click>
   <ThumbsSVG/>
+  <div class="counter">0</div>
 </div>
