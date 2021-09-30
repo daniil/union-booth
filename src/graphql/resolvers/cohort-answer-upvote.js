3084bf9afc7acf505a61965323cf83b3d3a3bd91
Daniil Molodkov <dmolodkov@brainstation.io>
2021-09-30 09:35:50 -0400

    Add toggleCohortAnswerUpvote resolver

--- src/graphql/resolvers/cohort-answer-upvote.js
+++ src/graphql/resolvers/cohort-answer-upvote.js
@@ -25,12 +25,46 @@ export default {
       async (_, { cohortAnswerId, isAdd }, { models, session }) => {
         try {
           const user = await models.User.findOne({
+            attributes: ['cohortId'],
             where: {
               id: session.user.id
             }
           });
 
-          
+          const answer = await models.CohortAnswer.findOne({
+            where: {
+              id: cohortAnswerId,
+              isInactive: false,
+            },
+            include: {
+              model: models.CohortQuestion,
+              attributes: ['cohortId']
+            }
+          });
+
+          if (user.cohortId !== answer['cohort-question'].cohortId) {
+            throw new UserInputError(`Can not ${isAdd ? 'add' : 'remove'} vote for the answer from different cohort`);
+          }
+
+          if (!answer) {
+            throw new UserInputError('Can not upvote inactive answer');
+          }
+
+          if (isAdd) {
+            await models.CohortAnswerUpvote.upsert({
+              userId: session.user.id,
+              cohortAnswerId
+            });
+          } else {
+            await models.CohortAnswerUpvote.destroy({
+              where: {
+                userId: session.user.id,
+                cohortAnswerId
+              }
+            });
+          }
+
+          return true;
         } catch(err) {
           throw new UserInputError(parseSequelizeError(err));
         }
