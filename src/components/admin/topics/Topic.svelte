418f24d6c811e87b5f5c1448461e8726ff09c62e
Daniil Molodkov <dmolodkov@brainstation.io>
2021-10-13 11:11:56 -0400

    Change admin topic component to use AdminLink

--- src/components/admin/topics/Topic.svelte
+++ src/components/admin/topics/Topic.svelte
@@ -1,15 +1,10 @@
 <script>
+  import AdminLink from 'components/admin/AdminLink.svelte';
+
   export let details;
 </script>
 
-<style lang="scss">
-  h4 {
-    font-size: 0.925rem;
-    margin-bottom: 0.875rem;
-    @media (min-width: 48rem) {
-      font-size: 1.1rem;
-    }
-  }
-</style>
-
-<h4>{details.title}</h4>
+<AdminLink
+  href="/admin/topics/{details.slug}"
+  title={details.title}
+/>
